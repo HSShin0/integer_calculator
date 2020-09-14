d0f035b6a631a690a73f211bf29b83ea90870b1c
HSShin0 <shin.hyungseok@gmail.com>
2020-09-14 16:00:12 +0900

    Add docstrings for calculatior module

--- calculator.py
+++ calculator.py
@@ -1,31 +1,52 @@
+# -*- coding: utf-8 -*-
+"""Basic calculation classes including add, sub, mul, and div.
+
+    Author: Hyungseok Shin
+    Contact: hsshin@jmarple.ai 
+"""
+
 from abc import ABC, abstractmethod
 
 class Calculator(ABC):
-
+    """An abstract class of basic computations."""
     @abstractmethod
     def operate(self, x:int, y:int) -> int:
+        """Operate the defined calcuation with two given operands.
+
+        Args:
+            x (int): left operand.
+            y (int): right operand.
+
+        Returns:
+            int: Calcalated result.
+
+        """
         pass  
 
 
 class Adder(Calculator):
-
+    """Addition."""
     def operate(self, x:int, y:int) -> int:
+        """Add two integers."""
         pass
 
 
 class Subtractor(Calculator):
-
+    """Subtraction."""
     def operate(self, x:int, y:int) -> int:
+        """Subtract two integers."""
         pass
 
 
 class Multiplier(Calculator):
-
+    """Muliplication."""
     def operate(self, x:int, y:int) -> int:
+        """Multiply two integers."""
         pass
 
 
 class Divider(Calculator):
-
+    """Division."""
     def operate(self, x:int, y:int) -> int:
+        """Divide two integers."""
         pass
